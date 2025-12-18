aa0c27d1c2553e85716883188697ab992bcad249
abecombe <55338725+abecombe@users.noreply.github.com>
2025-12-18 16:33:38 +0900

    Fix formatting and adjust quaternion initialization

--- svd.hlsl
+++ svd.hlsl
@@ -202,9 +202,9 @@ void SVD(in float3x3 A, out float3x3 U, out float3 S, out float3x3 V)
     float s12 = S_mat[1][2];
     float s22 = S_mat[2][2];
 
-    float4 q = float4(1, 0, 0, 0);
-
     float ch, sh;
+
+    float4 q = float4(1, 0, 0, 0);
 
     [unroll]
     for (int i = 0; i < 4; i++)
